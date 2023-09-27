9ff8ebcad6f55548377d9f731ec79ef492ad2aa6
olabola1108 <olabola1108@gmail.com>
2023-09-27 13:57:31 -0400

    backend created Contributing-to: https://olabola1108.atlassian.net/jira/software/projects/IP/boards/2? Problem Solution Note

---
+++ s3-backend/variable.tf
@@ -0,0 +1,22 @@
+variable "table_name" {
+  description = "The name of the DynamoDB table. Must be unique in this AWS account."
+  type        = string
+  default     = "terraform-remote-state-dynamo"
+}
+
+variable "aws_region" {
+  type        = string
+  description = "Region to create the global bucket"
+  default     = "us-west-2"
+}
+
+/*
+variable "versioning" {
+  type    = string
+  default = "Enabled"
+}
+*/
+variable "env" {
+  type    = list(string)
+  default = ["dev", "prod"]
+}
